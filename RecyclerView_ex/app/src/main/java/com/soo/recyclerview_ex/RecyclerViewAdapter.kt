bea75e51ae3ca686ee011ce016f6e464bc514461
kosy318 <kkosy1782@naver.com>
2022-05-19 23:37:58 +0900

    Create RecyclerView_ex

---
+++ RecyclerView_ex/app/src/main/java/com/soo/recyclerview_ex/RecyclerViewAdapter.kt
@@ -0,0 +1,21 @@
+package com.soo.recyclerview_ex
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdapter(val items: MutableList<String>): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
